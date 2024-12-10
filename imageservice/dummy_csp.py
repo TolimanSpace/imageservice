f7d260419247fa023806f64fd4cb8758ae8273ec
Tim White <tim.white@sydney.edu.au>
2024-12-10 17:10:58 +1100

    Function to send test settings and commands to image service

---
+++ imageservice/dummy_csp.py
@@ -0,0 +1,26 @@
+import time
+import PySpin
+
+# CSP Listener Function
+def csp_listener(shared_status):
+    camera_settings = {
+        'acquisitionmode': PySpin.AcquisitionMode_Continuous,
+        'exposure': 50,
+        'framerate': 10,
+    }
+    shared_status["camera_settings"] = camera_settings
+    shared_status["testing"] = True
+
+    shared_status["enable_camera"] = True
+    time.sleep(1)
+    shared_status["begin_imaging"] = True
+    time.sleep(10)
+    shared_status["begin_imaging"] = False
+    shared_status["enable_camera"] = False
+    time.sleep(10)
+    shared_status["enable_camera"] = True
+    time.sleep(1)
+    shared_status["begin_imaging"] = True
+    time.sleep(10)
+    shared_status["begin_imaging"] = False
+    shared_status["enable_camera"] = False
