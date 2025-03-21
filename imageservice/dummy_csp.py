574b9c78621d2f31786e19229f0569f0a5df2aef
Tim White <tim.white@sydney.edu.au>
2025-03-21 17:27:42 +1100

    Use 12 bit

--- imageservice/dummy_csp.py
+++ imageservice/dummy_csp.py
@@ -1,5 +1,10 @@
 import time
 import PySpin
+import logging
+
+_logger = logging.getLogger(__name__)
+
+OBSERVING_TIME = 300
 
 # CSP Listener Function
 def csp_listener(shared_status):
@@ -7,21 +12,41 @@ def csp_listener(shared_status):
         'acquisitionmode': PySpin.AcquisitionMode_Continuous,
         'exposure': 50,
         'framerate': 10,
-        'pixelformat': PySpin.PixelFormat_Mono8
+        'pixelformat': PySpin.PixelFormat_Mono12Packed
     }
     shared_status["camera_settings"] = camera_settings
     shared_status["testing"] = True
 
+    _logger.info(f"Stop compression process")
+    shared_status["begin_compression"] = False
+
     shared_status["enable_camera"] = True
+    _logger.info(f"Camera enambled")
     time.sleep(1)
+    _logger.info(f"Beginning imaging")
     shared_status["begin_imaging"] = True
-    time.sleep(10)
+    _logger.info(f"Imaging for {OBSERVING_TIME} seconds")
+    time.sleep(OBSERVING_TIME)
     shared_status["begin_imaging"] = False
+    _logger.info(f"Imaging stopped")
     shared_status["enable_camera"] = False
-    time.sleep(10)
-    shared_status["enable_camera"] = True
-    time.sleep(1)
-    shared_status["begin_imaging"] = True
-    time.sleep(10)
-    shared_status["begin_imaging"] = False
-    shared_status["enable_camera"] = False
+    _logger.info(f"Camera disabled")
+
+    # time.sleep(10)
+
+    # shared_status["enable_camera"] = True
+    # _logger.info(f"Camera enabled")
+    # time.sleep(1)
+    # _logger.info(f"Beginning imaging")
+    # shared_status["begin_imaging"] = True
+    # time.sleep(60)
+    # shared_status["begin_imaging"] = False
+    # _logger.info(f"Imaging stopped")
+    # shared_status["enable_camera"] = False
+    # _logger.info(f"Camera disabled")
+    
+    _logger.info(f"Begin compression process")
+    shared_status["begin_compression"] = True
+    
+    _logger.info("Initialize app shutdown")
+    shared_status["close_app"] = True
